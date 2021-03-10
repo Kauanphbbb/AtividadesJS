305d9b59c9c5f48820fc226020cada64dae174a7
kauanphbbb <kauanphbbb@gmail.com>
2021-03-10 15:10:36 -0300

    Segunda questão

---
+++ idadeEmDias.js
@@ -0,0 +1,10 @@
+// Escreva uma função que receba a idade de uma pessoa em anos e
+// retorne a mesma idade em dias. Considerando que o ano tem 365 dias
+
+function yearsInDays(year) {
+  return year * 365;
+}
+
+myAge = yearsInDays(25);
+
+console.log(myAge);
