05d5f27d7aa8cfedf8ffa1afa167fb31ff60fc0d
kauanphbbb <kauanphbbb@gmail.com>
2021-03-13 17:01:56 -0300

    Questão 5

---
+++ numMaiorIgual.js
@@ -0,0 +1,12 @@
+// Crie uma função que receba dois números e retorne se o primeiro é maior ou igual ao segundo.
+
+function numVerify(big, small) {
+  if (big >= small) {
+    return true;
+  }
+  return false;
+}
+
+big = numVerify(10, 10);
+
+console.log(big);
