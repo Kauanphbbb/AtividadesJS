2cdbc82df60fbd4c5920bbd5653cd71b763f5799
kauanphbbb <kauanphbbb@gmail.com>
2021-03-13 17:03:50 -0300

    Adicionando uma correção na questão 05

--- numMaiorIgual.js
+++ numMaiorIgual.js
@@ -1,12 +1,15 @@
 // Crie uma função que receba dois números e retorne se o primeiro é maior ou igual ao segundo.
 
 function numVerify(big, small) {
-  if (big >= small) {
-    return true;
+  if (big === small) {
+    if (big >= small) {
+      return true;
+    }
+  } else {
+    return false;
   }
-  return false;
 }
 
-big = numVerify(10, 10);
+big = numVerify(10, "10");
 
 console.log(big);
