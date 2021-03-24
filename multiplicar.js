d039f7915de10e239d5a26cf2106648de6ee2656
kauanphbbb <kauanphbbb@gmail.com>
2021-03-24 17:16:44 -0300

    Questão 08

---
+++ multiplicar.js
@@ -0,0 +1,16 @@
+// Desenvolva uma função que recebe dois números inteiros não negativos
+// (maiores ou iguais a zero) e realize a multiplicação deles.
+// Porém, não utilize o operador de mutiplicação.
+
+function mult(numA, numB) {
+  let result = 0;
+  let numMax = Math.max(numA, numB);
+  let numMin = Math.min(numA, numB);
+  for (let i = 0; i < numMin; i++) {
+    result += numMax;
+  }
+
+  return result;
+}
+
+console.log(mult(5, 10));
