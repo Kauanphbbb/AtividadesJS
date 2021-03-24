aa87c5ad59d5cd44728881fb3be87546ce49a014
kauanphbbb <kauanphbbb@gmail.com>
2021-03-24 16:57:29 -0300

    Refatorando o código

--- nomeMes.js
+++ nomeMes.js
@@ -3,35 +3,24 @@
 // retornar "fevereiro", pois este é o 2° mês.
 
 function nameMonth(num) {
-  if (num == 1) {
-    return "janeiro";
-  } else if (num == 2) {
-    return "fevereiro";
-  } else if (num == 3) {
-    return "março";
-  } else if (num == 4) {
-    return "abril";
-  } else if (num == 5) {
-    return "maio";
-  } else if (num == 6) {
-    return "junho";
-  } else if (num == 7) {
-    return "julho";
-  } else if (num == 8) {
-    return "agosto";
-  } else if (num == 9) {
-    return "setembro";
-  } else if (num == 10) {
-    return "outubro";
-  } else if (num == 11) {
-    return "novembro";
-  } else if (num == 12) {
-    return "dezembro";
-  } else {
-    return "mês inválido";
-  }
+  const monthNames = [
+    "janeiro",
+    "fevereiro",
+    "março",
+    "abril",
+    "maio",
+    "junho",
+    "julho",
+    "agosto",
+    "setembro",
+    "outubro",
+    "novembro",
+    "dezembro",
+  ];
+
+  return monthNames[--num]
 }
 
-const janeiro = nameMonth(1);
+const janeiro = nameMonth(2);
 
 console.log(janeiro);
